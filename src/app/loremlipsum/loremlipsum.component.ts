1d59e8d9736177d9b170c42a1e9adab8ca792b84
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2022-01-01 17:45:09 +0200

    lab 02c and 02d ready

--- src/app/loremlipsum/loremlipsum.component.ts
+++ src/app/loremlipsum/loremlipsum.component.ts
@@ -3,12 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-loremlipsum',
   template: `<hr />
-    <p>{{ loremlipsum }}</p>
-    <button>Reverse</button>
+    <p [ngClass]="{ disabled: computing }">{{ loremlipsum }}</p>
+    <button [disabled]="computing" (click)="reverse()">Reverse</button>
     <hr />`,
   styleUrls: ['./loremlipsum.component.css'],
 })
 export class LoremComponent {
+  computing: boolean = false;
+
+  reverse() {
+    this.computing = true;
+
+    setTimeout(() => {
+      this.loremlipsum = this.loremlipsum.split('').reverse().join('');
+      this.computing = false;
+    }, 1000);
+  }
+
   loremlipsum: string = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque cursus
       euismod nulla in porta. Aliquam sollicitudin neque nulla, sit amet dictum
       erat dictum a. Phasellus eget dui nisl. Vivamus tempus felis ut sodales
